8c1156728584309632a0e1730048a4298a5793a4
yogitaraval <ravalyogita5gmail.com>
2024-12-26 11:46:38 +0530

    updates

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app=express()
 app.use(cookieParser())
 app.use(express.json())
 app.use(cors({
-    origin: ["http://localhost:5173","http://localhost:5174","http://localhost:5175","http://localhost:5176"],
+    origin: ["https://blogfrontend-ln28.vercel.app","http://localhost:5173","http://localhost:5174","http://localhost:5175","http://localhost:5176"],
     credentials: true
 }))
 
